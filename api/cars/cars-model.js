b1dc6352ed5613d7ba9b91e9ec30c19442104d5b
John-Blackburn <JRBbusinesscontact@gmail.com>
2021-10-17 23:00:59 -0400

    MVP complete + seed stretch

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -1,11 +1,27 @@
+const db = require("../../data/db-config");
+
 const getAll = () => {
-  // DO YOUR MAGIC
-}
+	// DO YOUR MAGIC
+	return db("cars");
+};
 
-const getById = () => {
-  // DO YOUR MAGIC
-}
+const getById = (id) => {
+	// DO YOUR MAGIC
 
-const create = () => {
-  // DO YOUR MAGIC
-}
+	return db("cars").where("id", id).first();
+};
+
+const create = (newCar) => {
+	// DO YOUR MAGIC
+	console.log(newCar);
+	return db("cars")
+		.insert(newCar)
+		.then(() => {
+			return db("cars").where("vin", newCar.vin).first();
+		});
+};
+module.exports = {
+	getAll,
+	getById,
+	create,
+};
--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,41 @@
 // DO YOUR MAGIC
+const express = require("express");
+const Cars = require("./cars-model");
+const mw = require("./cars-middleware");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+	Cars.getAll()
+		.then((cars) => {
+			console.log(cars);
+			res.status(200).json(cars);
+		})
+		.catch((err) => res.status(500).json({ message: err.message }));
+});
+
+router.get("/:id", mw.checkCarId, (req, res) => {
+	const { id } = req.params;
+	Cars.getById(id)
+		.then((car) => {
+			res.status(200).json(car);
+		})
+		.catch((err) => res.status(500).json({ message: err.message }));
+});
+
+router.post(
+	"/",
+	mw.checkCarPayload,
+	mw.checkVinNumberValid,
+	mw.checkVinNumberUnique,
+	(req, res) => {
+		const newCar = req.body;
+		console.log(req.body);
+		Cars.create(newCar)
+			.then((car) => {
+				res.status(201).json(car);
+			})
+			.catch((err) => res.status(500).json({ message: err.message }));
+	}
+);
+
+module.exports = router;
